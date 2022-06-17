bb2c0fab2b6fe2899a3f156c8167888d87fe99ac
Brian Seaver <bseaver@d155.org>
2022-06-17 17:47:36 -0500

    Added TableView for Games. Added Game class Added Aset class

---
+++ Individual volleyball stats/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  Individual volleyball stats
+//
+//  Created by Brian Seaver on 6/17/22.
+//
+
+import Foundation
+
+class Game: Codable{
+    var gameName = ""
+    var athleteName  = ""
+    var sets = [ASet(), ASet(), ASet(), ASet(), ASet()]
+    
+  
+}
+
+class ASet: Codable{
+    var attack = 0
+    var kill = 0
+    var attackErr = 0
+    
+    var assistAtt = 0
+    var assist = 0
+    var ballHandErr = 0
+    
+    var soloBlock = 0
+    var blockAsst = 0
+    
+    var dig = 0
+    var serveAtt = 0
+    var serveErr = 0
+    var ace = 0
+}
--- Individual volleyball stats/ViewController.swift
+++ Individual volleyball stats/ViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var games = [Game]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell") as! UITableViewCell
+        if indexPath.row % 2 == 0{
+            cell.backgroundColor = UIColor.white
+        
+        }
+        else{
+            cell.backgroundColor = UIColor.lightGray
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toStats", sender: self)
     }
 
 
